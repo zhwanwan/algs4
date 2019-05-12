a305ff9032f6a2673f6bb9b8e6c797b8eb828bd4
wangzhen <334271937@qq.com>
2019-05-13 00:35:49 +0800

    5.4

--- src/main/java/zhwanwan/algs/SortUtils.java
+++ src/main/java/zhwanwan/algs/SortUtils.java
@@ -127,15 +127,20 @@ public class SortUtils {
 
     /**
      * 快速排序
+     * 1.找到分区点位置
+     * 2.对分区左侧快速排序
+     * 3.对分区右侧快速排序
      *
      * @param arr
-     * @param from
-     * @param to
      */
+    public static void quickSort(int arr[]) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
     private static void quickSort(int arr[], int from, int to) {
         if (from < to) { //递归结束的条件
             //1.分区
-            int base = partition(arr, from, to);
+            int base = partition2(arr, from, to);
             //2.对左边进行排序
             quickSort(arr, from, base - 1);
             //3.对右边进行排序
@@ -165,10 +170,94 @@ public class SortUtils {
         return i;
     }
 
-    public static void quickSort(int arr[]) {
-        quickSort(arr, 0, arr.length - 1);
+    private static int partition2(int a[], int lo, int hi) {
+        int i = lo, j = hi + 1, v = a[lo];
+        while (i < j) { //pointers not cross
+            //find item on lo to swap
+            while (a[++i] < v) {
+                if (i == hi)
+                    break;
+            }
+            //find item on hi to swap
+            while (v < a[--j]) {
+                if (j == lo)
+                    break;
+            }
+            swap(a, i, j);
+        }
+        swap(a, lo, j);
+        return j;
     }
 
+    public static int quickSelect(int a[], int k) {
+        int lo = 0, hi = a.length - 1;
+        while (hi > lo) {
+            int j = partition2(a, lo, hi);
+            if (j > k)
+                lo = j + 1;
+            else if (j < k)
+                hi = j - 1;
+            else
+                return a[k];
+        }
+        return a[k];
+    }
+
+    private static void swap(int a[], int i, int j) {
+        if (i >= j)
+            return;
+        a[i] = a[i] ^ a[j];
+        a[j] = a[j] ^ a[i];
+        a[i] = a[i] ^ a[j];
+    }
+
+
+    /**
+     * 堆排序-升序
+     * 1.Building heap using bottom-up method
+     * 2.
+     * --Remove the maximum, one at a time;
+     * --Leave in array,instead of nulling out
+     *
+     * @param arr
+     */
+    public static void heapSort(int arr[]) {
+        int n = arr.length;
+        //1.Heap construction:构造"最大堆"--从下往上
+        for (int k = n / 2; k >= 1; k--)
+            sink(arr, k, n);
+
+        //sort down
+        while (n > 1) {
+            exch(arr, 1, n--);
+            sink(arr, 1, n);
+        }
+    }
+
+    private static void sink(int a[], int k, int n) {
+        while (2 * k <= n) {
+            int j = 2 * k;
+            if (j < n && a[j - 1] < a[j])
+                j++;
+            if (a[k - 1] >= a[j - 1]) {
+                /**
+                 * 没有元素需要移动,停止处理
+                 * (只有在当前元素需要交换后,才需要检查后面的是否需要移动)
+                 */
+                break;
+            }
+            exch(a, k, j);
+            k = j;
+        }
+    }
+
+    private static void exch(int a[], int i, int j) {
+        if (i >= j)
+            return;
+        a[i - 1] = a[i - 1] ^ a[j - 1];
+        a[j - 1] = a[j - 1] ^ a[i - 1];
+        a[i - 1] = a[i - 1] ^ a[j - 1];
+    }
 
     public static void main(String args[]) {
         int arr[] = {72, 6, 57, 88, 60, 42, 83, 43, 48, 85, 3, 5, 45, 32, 11, 2, 1, 99, 71};
@@ -177,7 +266,9 @@ public class SortUtils {
 //        selectSort(arr);
 //        insertSort(arr);
 //        shellSort(arr);
-        mergeSort(arr);
+//        swap(arr, 0, 1);
+        System.out.println(quickSelect(arr, 3));
+        heapSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
